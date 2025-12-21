f244b8afa52a2ba5666005a535f375159923a895
Manraaj Singh <manraajsingh2006@gmail.com>
2025-12-21 03:48:09 -0500

    Look

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -7,7 +7,7 @@ export const RESUME_DATA = {
   locationLink: "https://www.google.com/maps/place/Boston, Massachusetts",
   about: "CS & Applied Math at Dartmouth College",
   summary:
-    "Studying CS and Math @ Dartmouth College. Interested in product design, machine learning, and backend eng. I ship fast and take ideas to execution. \nCurrently building something cool in computer vision + sports ;)",
+    "Engineer @ Whop \nStudying CS and Math @ Dartmouth College (On leave). Interested in product design, machine learning, and backend eng. I ship fast and can take ideas to execution. \nCurrently building something cool on the side in computer vision + sports ;)",
   avatarUrl: "/profile.jpg",
   personalWebsiteUrl: "https://github.com/m4nr44j",
   resumeUrl: "/resume.pdf",
