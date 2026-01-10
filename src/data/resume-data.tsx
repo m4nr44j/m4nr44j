70d3c98413d081d479284f1cb16394fcaeb6b049
Manraaj Singh <manraaj.singh@whop.com>
2026-01-09 17:55:12 -0800

    Updates

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -5,9 +5,9 @@ export const RESUME_DATA = {
   initials: "MS",
   location: "Palo Alto, CA",
   locationLink: "https://www.google.com/maps/place/Palo Alto, California",
-  about: "Building @ Whop",
+  about: "Engineering @ Whop",
   summary:
-    "Product Engineer @ Whop working on agentic workflows for e-commerce. \n Exited Founder, Clio (Acq. by Whop, 8-figures)\nStudying CS & Math @ Dartmouth College (On leave). \nCurrently building something cool on the side in computer vision + sports ;) \nInterested in product design, machine learning, and backend eng. I ship fast and can take ideas to execution.",
+    "Product Engineer @ Whop. \n Exited Founder, Clio (Acq. by Whop)\nStudying CS & Math @ Dartmouth College (On leave). \nCurrently building something cool on the side in computer vision + sports ;) \nInterested in product design, machine learning, and backend eng. I ship fast and can take ideas to execution.",
   avatarUrl: "/profile.jpg",
   personalWebsiteUrl: "https://github.com/m4nr44j",
   resumeUrl: "/resume.pdf",
